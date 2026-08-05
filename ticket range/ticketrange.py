52b372e3ee488d3479ea4eb6d2a65ffc32fc7619
ronakchouhan0205-gif <ronakchouhan0205@gmail.com>
2026-08-05 17:14:54 +0530

    Add ticket pricing logic based on age and day

---
+++ ticket range/ticketrange.py
@@ -0,0 +1,13 @@
+age=int(input("Enter your age: "))
+days=str(input("Enter the day of the week: "))
+weekend=["saturday","sunday"]
+if(days in weekend):
+    print("Your ticket price is $300")
+elif(age>=1 and age<=11):
+    print("Your ticket price is $110")
+elif(age>=12 and age<=59):
+    print("Your ticket price is $250")
+elif(age>=60):
+    print("Your ticket price is $150")
+elif(age<=0):
+    print("Invalid age entered")
