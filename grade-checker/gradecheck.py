5dd70eeecb2b3c315db135129fd24a5365df5eb5
ronakchouhan0205-gif <ronakchouhan0205@gmail.com>
2026-08-05 17:03:04 +0530

    Add grade checker functionality in gradecheck.py
    Implement grade checking based on user input marks.

---
+++ grade-checker/gradecheck.py
@@ -0,0 +1,12 @@
+    marks = int(input("Enter your marks: "))
+
+    if(marks >=90 and marks<=100):
+        print("A+")     
+    elif(marks >=75 and marks<=89 ):
+        print("B+")
+    elif(marks>=60 and marks<=74):
+        print("C+")
+    elif(marks>=40 and marks<=59):
+        print("D+") 
+    elif(marks >=0 and marks<=39):
+        print("F")
